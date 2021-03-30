4758334b4b1105f9ef7f1f24bc179c3c19f0597d
shreypatel30 <shrey.patel@students.jmcss.org>
2021-03-30 08:51:13 -0500

    added main.js style.css edits, gallery edits

---
+++ main.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("zoom");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+for (i = 0, len = img.length; i < len; i++){
+    img[i].onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+      }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
