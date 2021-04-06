6a5c56e5260631f4c1ed3fcf05bae1733b7f76d5
Michael Crockett <77060840+michael-school@users.noreply.github.com>
2021-04-06 07:52:01 -0500

    scroll to top button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 //scroll to top
+
+//Get the button:
+scrollButton = document.getElementById("scrollButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {showButton()};
+
+function showButton() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
