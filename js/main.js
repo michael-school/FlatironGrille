1c7e0d0e83772a693434b59d4487afcb94e50c60
Michael Crockett <77060840+michael-school@users.noreply.github.com>
2021-04-12 08:51:08 -0500

    finished event content

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 //Get the button:
 scrollButton = document.getElementById("scrollButton");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 1000px from the top of the document, show the button
 window.onscroll = function() {showButton()};
 
 function showButton() {
