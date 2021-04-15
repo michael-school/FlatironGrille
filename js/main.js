89c24d363ca0ec0d62d0d68890539c4e1341bd5f
Michael Crockett <77060840+michael-school@users.noreply.github.com>
2021-04-15 08:22:21 -0500

    styled events

--- js/main.js
+++ js/main.js
@@ -21,8 +21,3 @@ function scrollToTop() {
 }
 
 
-//initialize tooltips
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
-});
